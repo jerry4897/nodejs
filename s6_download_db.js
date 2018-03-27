5309783f88e92ccdca386637a46b05591912e4e8
SukHwan Kim <30820468+jerry4897@users.noreply.github.com>
2018-03-27 19:02:20 +0900

    Update s6_download_db.js

--- s6_download_db.js
+++ s6_download_db.js
@@ -10,6 +10,28 @@ var connection = mysql.createConnection({
 })
 connection.connect();
 
+function insert_sensor(device, unit, type, value, seq, ip) {
+  obj = {};
+  obj.seq = seq;
+  obj.device = device;
+  obj.unit = unit;
+  obj.type = type;
+  obj.value = value;
+  obj.ip = ip.replace(/^.*:/, '')
+
+  var query = connection.query('insert into sensors set ?', obj, function(err, rows, cols) {
+    if (err) throw err;
+    console.log("database insertion ok= %j", obj);
+  });
+}
+
+app.get('/log', function(req, res) {
+  r = req.query;
+  console.log("GET %j", r);
+
+  insert_sensor(r.device, r.unit, r.type, r.value, r.seq, req.connection.remoteAddress);
+  res.end('OK:' + JSON.stringify(req.query));
+});
 
 app.get("/data", function(req, res) {
   console.log("param=" + req.query);
@@ -30,6 +52,30 @@ app.get("/data", function(req, res) {
     res.send(html);
   });
 
+});
+
+app.get("/download", function(req,res) {
+	var qstr = 'select * from sensors ';
+	var http = require('http');
+	var fs = require('fs');
+	var request = http.get("http://163.239.76.204:8083/data", function(response) {
+		if (response.statusCode === 200) {
+			var file1 = fs.createWriteStream("data.csv");
+			response.pipe(file1);
+		}
+		request.setTimeout(12000, function() {
+			request.abort();
+		});
+	}); 
+	connection.query(qstr, function(err, rows, cols) {
+		if(err) {
+			throw err;
+			res.send('query error: '+ qstr);
+			return;
+		}
+	});		
+	var file = __dirname + '/data.csv';
+	res.download(file);	
 });
 
 var server = app.listen(8083, function () {
